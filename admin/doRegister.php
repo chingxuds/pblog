ea2783e02060095952408105b653daf239816498
chingxuds <chingxuds@gmail.com>
2013-03-27 17:00:08 +0800

    添加了kindeditor和jQuery插件
    在项目中添加了用于创建编辑框的kindeditor插件和著名的jQuery插件。

--- admin/doRegister.php
+++ admin/doRegister.php
@@ -1,13 +1,18 @@
 <?php
 require './includes/encrypt.php';
+require './includes/dateformat.php';
 
 $username_input = $_POST['username_input'];
 $userpass_input = $_POST['userpass_input'];
 $nicename_input = $_POST['nicename_input'];
 $useremail_input = $_POST['useremail_input'];
 
+
 $userpass_coded = code_md5($userpass_input);
 
 echo $username_input . '||' . $userpass_input . '||' . $nicename_input . '||' . $useremail_input . '<br />';
-echo $userpass_coded;
+echo $userpass_coded . '<br />';
+
+$register_time_gmt = dateFormat($_SERVER['REQUEST_TIME']);
+echo $register_time_gmt;
 ?>
---
+++ admin/includes/dateformat.php
@@ -0,0 +1,13 @@
+<?php
+$date_format = 'Y-m-d H:i:s';
+$server_timezone = 'Asia/Shanghai';
+
+function dateFormat($time='0')
+{
+	return date($GLOBALS['date_format'],$time);
+}
+
+echo dateFormat(time());
+echo "<br />";
+echo dateFormat();
+?>
