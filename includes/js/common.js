d2f12d4dc1be62f19b1693c607c6a5a21bb52302
chingxuds <chingxuds@gmail.com>
2013-04-02 20:54:22 +0800

    设计了几个前台页面的样式
    设计了首页、文章页、搜索页的样式；
    更新了css和js文件。

--- includes/js/common.js
+++ includes/js/common.js
@@ -2,3 +2,12 @@ function getCurrentFormatDate() {
 	var d = new Date();
 	return d.getFullYear() + "-" + (d.getMonth() + 1) + "-" + d.getDate() + " " + d.getHours() + ":" + d.getMinutes() + ":" + d.getSeconds();
 }
+
+$(window).scroll(function() {
+  	$('#div_top_bar').css('top', $(document).scrollTop());
+});
+
+$(function() {
+$( "#button_search" ).button();
+$("#nav_menu_ul").buttonset();
+});
